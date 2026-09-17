48ec3157d522b428f304cd51b9fdd676e3106646
Rio Saputra <54255518+riosaputra29@users.noreply.github.com>
2026-09-18 01:43:36 +0700

    Remove buildCodeBlockHtml function
    Removed the buildCodeBlockHtml function that generates HTML for code blocks.

--- public/js/app.js
+++ public/js/app.js
@@ -201,54 +201,6 @@ function highlightCode(lang, code){
 // CODE BLOCK
 // =========================================================
 
-function buildCodeBlockHtml(
-  block.lang,
-  block.code
-
-){
-
-  const label =
-    lang
-      ? lang.toUpperCase()
-      : 'CODE';
-
-  const highlighted =
-    highlightCode(
-      lang,
-      code
-    );
-
-  return (
-
-    '<div class="code-block">' +
-
-      '<div class="code-block-header">' +
-
-        '<span class="code-lang">' +
-          escapeHtml(label) +
-        '</span>' +
-
-        '<button ' +
-          'class="code-copy-btn" ' +
-          'type="button">' +
-
-          COPY_ICON_SVG +
-
-          '<span>Salin</span>' +
-
-        '</button>' +
-
-      '</div>' +
-
-      '<pre><code class="hljs">' +
-        highlighted +
-      '</code></pre>' +
-
-    '</div>'
-
-  );
-}
-
 
 function stopAIThinking(aiBubble){
 
